3eb7888ab1fedebbdc88c01f199d8190fb1728d3
minhalo113 <minha@MSI>
2025-11-28 14:46:11 -0700

    remove dotenv

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-import dotenv from "dotenv"
-import process from "process"
-dotenv.config();
+// import dotenv from "dotenv"
+// import process from "process"
+// dotenv.config();
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api"
